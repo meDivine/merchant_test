efde18f2e6c021cb2c5fc07d4af6a012e8537c4c
Aleksandr <snowstormz@yandex.ru>
2023-11-16 16:47:46 +0100

    migration schema for payments

---
+++ database/migrations/2023_11_14_150243_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+            $table->unsignedBigInteger('merchant_id');
+            $table->integer('payment_id');
+            $table->string('status', 32);
+            $table->integer('amount');
+            $table->integer('amount_payment');
+            $table->timestamp('timestamp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
